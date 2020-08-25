5783c8f4b37424e103b424adb226e57c14f8e938
XavierVG <kjviervelasque16@gmail.com>
2020-08-25 18:41:24 -0500

    primera subida

---
+++ server.js
@@ -0,0 +1,10 @@
+
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname+'/dist/example'));
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/example/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
